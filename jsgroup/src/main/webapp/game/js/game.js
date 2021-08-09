a995446ec39fab9ad22f53730ac60dee28724242
JiHyeonBeak <84897199+JiHyeonBeak@users.noreply.github.com>
2021-08-09 19:29:57 +0900

    자바스크립트 : 미니타자게임 완성본

--- jsgroup/src/main/webapp/game/js/game.js
+++ jsgroup/src/main/webapp/game/js/game.js
@@ -1,7 +1,7 @@
 /**
  * 
  */
-	var ex1 = ['jelly','dragon','hambuger','rice','xylitol','lupine','korea','paradise','jupiter','block','significnat'
+	var ex1 = ['jelly','dragon','hambuger','rice','xylitol','lupine','korea','paradise','jupiter','block','significant'
 				,'sequence','cohesion','sherlock','module','prototype'];
 	var score =0;
 	var num= parseInt((Math.random()*15)+0);
@@ -11,19 +11,22 @@
 	console.log("초기 반복값:"+num);
 	
 	function next() {
-		$('info').hide();
-		//$('first').show();
-/*		document.getElementById('info').removeAttribute('class');
+		document.getElementById('info').removeAttribute('class');
 		document.getElementById('info').setAttribute('class','active');
 		
-		document.getElementById('first').removeAttribute('class');
-		document.getElementById('first').setAttribute('class','active');*/
+		document.getElementById('stt').setAttribute('class','active');
+		document.getElementById('nxt').setAttribute('class','active');
 		
 	}
 	
 	
 	
 	function st(){  //시작키를 누를 시
+		document.getElementById('stt').removeAttribute('class');
+		document.getElementById('nxt').setAttribute('class','active');
+		
+		document.getElementById('first').setAttribute('class','active');
+	
 		document.getElementById('info').removeAttribute('class');
 		document.getElementById('ex').innerText = ex1[num];
 		document.getElementById('sco').innerText = '점수: '+score;
@@ -41,17 +44,31 @@
 	
 	function ent(){  //확인 키를 누를때마다
 		var user = document.getElementById('txt').value;
+
 		++count;
 		console.log('유저 : '+user);
 		console.log('코렉트 : '+ex1[num]);
 			
 			if(user === ex1[num]) {
+				document.getElementById('sad').removeAttribute('class');
+				document.getElementById('awsome').removeAttribute('class');
+				document.getElementById('awsome').setAttribute('class','active');
+				document.getElementById('comment').innerText='멋져!';
+				
+				
 				if(sc > 3) {
 					score++;
 				}
 				else {
 					score = score+2;
 				}
+		}
+			else {
+				document.getElementById('awsome').removeAttribute('class');
+				document.getElementById('sad').removeAttribute('class');
+				document.getElementById('sad').setAttribute('class','active');
+				document.getElementById('comment').innerText='잘 해보자';
+			
 		}
 		num = parseInt((Math.random()*15)+0);
 		document.getElementById('sco').innerText = '점수: '+score;
@@ -63,16 +80,22 @@
 		console.log('현재단어: '+ex1[num]);
 		console.log('카운트 수: '+count);
 		
-		 if(count === 8 ) {
+		 if(count === 10 ) {
+			document.getElementById('first').removeAttribute('class');
+			document.getElementById('tm').setAttribute('class','active');
 			
-			if(score > 10) {
-				document.getElementById('sco').innerText = '아주 멋져요! '+ score + '점 입니다.';
+			
+			if(score >= 10) {
+				document.getElementById('ex').innerText = '아주 멋져요! '+ score + '점 입니다.';
+				document.getElementById('comment').innerText='대단해!';
 			}
 			else if(9 > score > 6) {
-				document.getElementById('sco').innerText = '잘 하시네요! '+score+'점 입니다.';
+				document.getElementById('ex').innerText = '잘 하시네요! '+score+'점 입니다.';
+				document.getElementById('comment').innerText='잘 하는데!';
 			}
-			else if(score < 6) {
-				document.getElementById('sco').innerText = '연습할까요? ' +score+'점 입니다.';
+			else if(score <= 6) {
+				document.getElementById('ex').innerText = '연습할까요? ' +score+'점 입니다.';
+				document.getElementById('comment').innerText='노력해보자!';
 			}
 	}
 		sc = 0;
